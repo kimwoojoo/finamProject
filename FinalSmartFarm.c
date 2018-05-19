e32ef52c14225fd22e5ce858e1db3ecb3d226171
= <kyr1217@gmail.com>
2018-05-19 16:30:01 +0000

    second

--- FinalSmartFarm.c
+++ FinalSmartFarm.c
@@ -1,4 +1,3 @@
-
 #include <wiringPi.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,28 +7,21 @@
 #include <wiringPiSPI.h>
 #include <signal.h>
 #include <mysql/mysql.h>
-
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 #include <time.h>
 #include <math.h>
 
 #define CS_MCP3208 8       // BCM_GPIO_8
-
 #define SPI_CHANNEL 0
 #define SPI_SPEED   1000000   // 1MHz
-
 #define VCC         4.8       // Supply Voltage
 
 // define farm 
 #define MAXTIMINGS 85
 #define RETRY 5
-
-//#define PUMP	21 // BCM_GPIO 5
-
 #define FAN	6 // BCM_GPIO 6
 #define MOTORCONTROL 5 // BCM_GPIO 13
 #define RGBLEDPOWER  24 //BCM_GPIO 19
@@ -39,24 +31,20 @@
 
 #define LIGHTSEN_OUT 2  //gpio27 - J13 connect
 
+//Thread에 값을 저장시킬 구조체
 typedef struct __myarg_t {
 	int a;
 	int b;
 }myarg_t;
-
 void sig_handler(int sig);
 int ret_humid, ret_temp;
-
 static int DHTPIN = 7;
 static int dht22_dat[5] = {0,0,0,0,0};
-
 int read_dht22_dat_temp();
-int read_dht22_dat_humid();
-int get_temperature_sensor();
-int get_humidity_sensor();
+
 int get_light_sensor();
 
-// here
+// MYSQL CONNECTOR 선언
 #define MAX 100000
 #define DBHOST "115.68.228.55"
 #define DBUSER "root"
@@ -66,12 +54,15 @@ int get_light_sensor();
 MYSQL *connector;
 MYSQL_RES *result;
 MYSQL_ROW row;
-int buffer[MAX];
-int buffer2[MAX];
+
+int buffer[MAX]; //temperature 값저장시킬buffer
+int buffer2[MAX]; //light  값저장시킬buffer
+
 int fill_ptr = 0;
 int use_ptr = 0;
 int count = 0;
 
+//signal handler 구현
 void sig_handler(int sig)
 {
 	digitalWrite(FAN,0);
@@ -80,6 +71,8 @@ void sig_handler(int sig)
 	digitalWrite(BLUE, 0);
 	exit(0);
 }
+
+//Data 저장시키는 함수
 void put(int value,int value2)
 {
 	buffer[fill_ptr] = value;
@@ -87,26 +80,32 @@ void put(int value,int value2)
 	fill_ptr = (fill_ptr + 1) % MAX;
 	count++;
 }
-
-myarg_t get()
+//Data 가져오는 함수
+myarg_t get(int real_ptr)
 {
 	myarg_t m1;
-	m1.a = buffer[use_ptr];
-	m1.b = buffer2[use_ptr];
-	use_ptr = (use_ptr + 1 ) %MAX;
+	m1.a = buffer[real_ptr];
+	m1.b = buffer2[real_ptr];
 	count--;
 	return m1;
 }
 
+//Thread condition 선언
 pthread_cond_t pro,con,TurnFan, TurnLED;
-pthread_mutex_t mutex, mutex2, mutex3;
+//Thread Mutex 언선
+pthread_mutex_t mutex;
 
+
+//Fan을 동작시키는 Thread
 void *TurnOnFAN(void *arg)
 {
+	//Thread를 wait시키고 signal이오면 동작
 	while(1)
 	{
-		pthread_cond_wait(&TurnFan, &mutex2);
-		printf("나 는팬이야\n");
+		pthread_mutex_lock(&mutex);
+		pthread_cond_wait(&TurnFan, &mutex);
+		pthread_mutex_unlock(&mutex);
+		printf("Fan ON\n");
 		digitalWrite(FAN,1);
 		sleep(5);
 		digitalWrite(FAN,0);
@@ -117,7 +116,9 @@ void *TurnONLED(void *arg)
 {
 	while(1)
 	{
-		pthread_cond_wait(&TurnLED, &mutex3);
+		pthread_mutex_lock(&mutex);
+		pthread_cond_wait(&TurnLED, &mutex);
+		pthread_mutex_unlock(&mutex);
 		digitalWrite(RED,1);
 		digitalWrite(GREEN,0);
 		digitalWrite(BLUE,0);
@@ -156,7 +157,7 @@ void *producer(void *arg)
 		if(tempValue >= 28)
 			pthread_cond_signal(&TurnFan);
 
-		if(lightValue >= 1000)
+		if(lightValue <= 500)
 			pthread_cond_signal(&TurnLED);
 		
 		else
@@ -174,7 +175,7 @@ void *consumer(void *arg)
 {
 	int i;
 	myarg_t m;
-	
+
 	for(i = 0 ; i< MAX; i++)
 	{
 		delay(10000);
@@ -182,10 +183,10 @@ void *consumer(void *arg)
 
 		while(count == 0)
 			pthread_cond_wait(&con, &mutex);
-
-		m = get();
-
-		printf("I am consumer: fan %d %d \n", m.a, m.b);
+		
+		m = get(fill_ptr - 1);
+		
+		printf("I am consumer: Temp light %d %d \n", m.a, m.b);
 		
 		connector = mysql_init(NULL);
 		if(!mysql_real_connect(connector, DBHOST, DBUSER, DBPASS, DBNAME, 3306, NULL, 0))
@@ -202,7 +203,7 @@ void *consumer(void *arg)
 			fprintf(stderr, "%s\n", mysql_error(connector));
 			printf("Write DB error \n");
 		}
-		printf("값넣었다\n");
+		printf("Data Stored\n");
 		pthread_cond_signal(&pro);
 		pthread_mutex_unlock(&mutex);
 	}
@@ -233,7 +234,7 @@ int main (void)
 {
 	signal(SIGINT, (void*)sig_handler);
   int adcChannel  = 0;
-  int adcValue[8] = {0};
+
 
   if(wiringPiSetup() == -1)
 	  exit(EXIT_FAILURE);
@@ -297,30 +298,7 @@ int main (void)
   return 0;
 }
 
-int get_temperature_sensor()
-{
-	int received_temp;
-	int _retry = RETRY;
 
-	DHTPIN = 11;
-
-	if (wiringPiSetup() == -1)
-		exit(EXIT_FAILURE) ;
-	
-	if (setuid(getuid()) < 0)
-	{
-		perror("Dropping privileges failed\n");
-		exit(EXIT_FAILURE);
-	}
-	while (read_dht22_dat_temp() == 0 && _retry--)
-	{
-		delay(500); // wait 1sec to refresh
-	}
-	received_temp = ret_temp ;
-	printf("Temperature = %d\n", received_temp);
-	
-	return received_temp;
-}
 
 static uint8_t sizecvt(const int read)
 {
@@ -403,107 +381,6 @@ int read_dht22_dat_temp()
     printf("Data not good, skip\n");
     return 0;
   }
-}
-
-
-int get_humidity_sensor()
-{
-	int received_humid;
-	int _retry = RETRY;
-
-	DHTPIN = 11;
-
-	if (wiringPiSetup() == -1)
-		exit(EXIT_FAILURE) ;
-	
-	if (setuid(getuid()) < 0)
-	{
-		perror("Dropping privileges failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while (read_dht22_dat_humid() == 0 && _retry--) 
-	{
-		delay(500); // wait 1sec to refresh
-	}
-
-	received_humid = ret_humid;
-	printf("Humidity = %d\n", received_humid);
-	
-	return received_humid;
-}
-
-int read_dht22_dat_humid()
-{
-	uint8_t laststate = HIGH;
-	uint8_t counter = 0;
-	uint8_t j = 0, i;
-
-	dht22_dat[0] = dht22_dat[1] = dht22_dat[2] = dht22_dat[3] = dht22_dat[4] = 0;
-
-	// pull pin down for 18 milliseconds
-	pinMode(DHTPIN, OUTPUT);
-	digitalWrite(DHTPIN, HIGH);
-	delay(10);
-	digitalWrite(DHTPIN, LOW);
-	delay(18);
-	// then pull it up for 40 microseconds
-	digitalWrite(DHTPIN, HIGH);
-	delayMicroseconds(40); 
-	// prepare to read the pin
-	pinMode(DHTPIN, INPUT);
-
-	// detect change and read data
-	for ( i=0; i< MAXTIMINGS; i++) 
-	{
-		counter = 0;
-		while (sizecvt(digitalRead(DHTPIN)) == laststate) 
-		{
-			counter++;
-			delayMicroseconds(1);
-			if (counter == 255) {
-			break;
-		}
-    }
-    laststate = sizecvt(digitalRead(DHTPIN));
-
-    if (counter == 255) break;
-
-    // ignore first 3 transitions
-    if ((i >= 4) && (i%2 == 0)) 
-	{
-		// shove each bit into the storage bytes
-		dht22_dat[j/8] <<= 1;
-		if (counter > 16)
-			dht22_dat[j/8] |= 1;
-		j++;
-    }
-}
-
-  // check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
-  // print it out if data is good
-	if ((j >= 40) && 
-      (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) ) {
-        float t, h;
-		
-        h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
-        h /= 10;
-        t = (float)(dht22_dat[2] & 0x7F)* 256 + (float)dht22_dat[3];
-        t /= 10.0;
-        if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
-		
-		ret_humid = (int)h;
-		ret_temp = (int)t;
-		printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t );
-		printf("Humidity = %d Temperature = %d\n", ret_humid, ret_temp);
-		
-    return ret_humid;
-	}
-	else
-	{
-		printf("Data not good, skip\n");
-		return 0;
-	}
 }
 
 int wiringPicheck(void)
